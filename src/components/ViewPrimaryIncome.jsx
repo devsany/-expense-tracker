ffbd5aa6aaf6228af1d8751740b865d48eaf4966
devsany <sunnyrajbodhgaya19@gmail.com>
2024-11-07 11:24:40 +0530

    add teh correct route of FAQ and logout

--- src/components/ViewPrimaryIncome.jsx
+++ src/components/ViewPrimaryIncome.jsx
@@ -315,7 +315,7 @@ const ViewPrimaryIncome = () => {
               <li>
                 <NavLink
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                  to={`/home`}
+                  to={`/F&Q`}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -336,7 +336,7 @@ const ViewPrimaryIncome = () => {
               <li>
                 <NavLink
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                  to={`/home`}
+                  to={`/`}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
--- src/components/expences/ExpencesInput.jsx
+++ src/components/expences/ExpencesInput.jsx
@@ -352,7 +352,7 @@ const ExpencesInput = () => {
               <li>
                 <NavLink
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                  to={`/home`}
+                  to={`/F&Q`}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -373,7 +373,7 @@ const ExpencesInput = () => {
               <li>
                 <NavLink
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                  to={`/home`}
+                  to={`/`}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
